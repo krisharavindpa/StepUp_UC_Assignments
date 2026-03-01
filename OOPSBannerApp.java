48e30633128b48ed664e3f1165605a8d2555d3d3
kp7227-srm <kp7227@srmist.edu.in>
2026-03-01 17:09:42 +0530

    UC1: print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+package StepUp_UC_Assignments;
+
+/**
+ * OOPSBannerApp UC1 – OOPS print
+ *
+ * * @author Krish Aravind Pandiyarajan
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
