3ee82192cd9be93ecdfe844e7bbdfb91d608e46f
kp7227-srm <kp7227@srmist.edu.in>
2026-03-01 17:17:18 +0530

    UC2: 7_line astericks banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,23 @@
 package StepUp_UC_Assignments;
 
 /**
- * OOPSBannerApp UC1 – OOPS print
- *
- * * @author Krish Aravind Pandiyarajan
- * @version 1.0
+ * OOPSBannerApp UC2 – Render OOPS as Banner using Print Statements
+        -OOPS made using asterisks
+ * * @author Krish Aravind
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("  *****    " +"  *****  "+"  ******   "+"   *****  ");
+        System.out.println(" **   **   " +" **   ** "+"  **   **  "+" **    ** ");
+        System.out.println("**     **  " +"**     **"+"  **   **  "+" **       ");
+        System.out.println("**     **  " +"**     **"+"  ******   "+"  ******  ");
+        System.out.println("**     **  " +"**     **"+"  **       "+"       ** ");
+        System.out.println(" **   **   " +" **   ** "+"  **       "+" **     **");
+        System.out.println("  *****    " +"  *****  "+"  **       "+"  ******  ");
+        
+
     }
 }
